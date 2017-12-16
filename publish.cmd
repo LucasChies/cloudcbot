b3703891cdd159533adebde7ea2d71d946e35757
CloudC Consultoria <lchies@gmail.com>
2017-12-16 10:09:03 -0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=cloudcbot-Web-Deploy.pubxml -p:Password=ZF6dxSDGaH0Dt0wPiFFPGr48crMn340JjvqGKCSlhfdQ6dRa05aEhCciaKrb
+
